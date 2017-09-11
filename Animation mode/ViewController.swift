ffa68f8344aef039f7a6795a0633db6b170c19cf
Vitalik Maksymliuk <makvitalik@gmail.com>
2017-09-11 22:14:47 +0300

    Added some animation with the rocket and ability to play song at the time when a rocket is start launching

--- Animation mode/ViewController.swift
+++ Animation mode/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocketImg: UIImageView!
+    @IBOutlet weak var rocketStateLbl: UILabel!
+    @IBOutlet weak var turnStateLbl: UILabel!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let path = Bundle.main.path(forResource: "rocketOn", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func powerBtnPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerBtn.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 3, animations: {
+            
+            self.rocketImg.frame = CGRect(x: 0, y: 280, width: 375, height: 125)
+            
+        }) { (finished) in
+            
+            self.rocketStateLbl.isHidden = false
+            self.turnStateLbl.isHidden = false
+        }
     }
-
+    
 
 }
 
